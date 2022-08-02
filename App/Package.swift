e9358cdbd92a114e346cdbb62a42cb28d605bb69
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-02 07:16:02 -0400

    App/Vinux.xcodeproj/project.pbxproj: add Starscream to macOS and iOS

--- App/Package.swift
+++ App/Package.swift
@@ -1,4 +1,4 @@
 dependencies: [
-    .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4),
+    .Package(url: "https://github.com/daltoniam/Starscream.git", branch: "v4.0.4"),
     .Package(url: "https://github.com/jb55/secp256k1.swift.git", branch: "main")
 ]
