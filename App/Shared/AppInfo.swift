e9ca81006c852ee56cb62ef15f95d6f259f6d854
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-08 16:47:29 -0400

    Vinux.xcodeproj/project.pbxproj: follow up AppInfo.swift config

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift
+// AppInfo.swift on Aug 8 16:38:39 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "777dd9b"
+let gitCommitSHA: String = "2120c4e"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
