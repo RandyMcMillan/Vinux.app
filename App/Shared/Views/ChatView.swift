edd0faca4f95777545d0298aea64ca51204f427a
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-08 01:52:40 -0400

    App/Shared/Views/ChatView.swift: background Color opacity 0.04

--- App/Shared/Views/ChatView.swift
+++ App/Shared/Views/ChatView.swift
@@ -121,7 +121,7 @@ struct ChatView: View {
                 .padding(6)
             }
             .padding([.leading], 2)
-            .background(Color.secondary.opacity(0.1))
+            .background(Color.secondary.opacity(0.04))
             .cornerRadius(8.0)
             
             //.border(Color.red)
