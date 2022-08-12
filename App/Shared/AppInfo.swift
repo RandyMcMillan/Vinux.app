2edcd136de791020920452253eaa076539ec8fd9
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-12 01:18:03 -0400

    Vinux (iOS) Catalyst config validated

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift hu Aug 11 04:26:55 2022 -0400
+// AppInfo.swift ri Aug 12 01:07:28 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "4852665"
+let gitCommitSHA: String = "42e8860"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
