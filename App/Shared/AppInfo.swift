42e88605c5413509e6fbd2b6fd98fa9f8c5d1c2c
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-12 01:07:28 -0400

    Vinux (iOS) validating - good config

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift on Aug 8 16:38:39 2022 -0400
+// AppInfo.swift hu Aug 11 04:26:55 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "2120c4e"
+let gitCommitSHA: String = "4852665"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
