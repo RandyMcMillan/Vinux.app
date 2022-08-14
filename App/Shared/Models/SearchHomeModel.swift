c54c575034dbb00d3095a7a0683effa802d0589d
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-14 02:42:42 +0100

    SearchHomeModel.swift: sync with damus

--- App/Shared/Models/SearchHomeModel.swift
+++ App/Shared/Models/SearchHomeModel.swift
@@ -14,16 +14,17 @@ class SearchHomeModel: ObservableObject {
     @Published var loading: Bool = false
 
     var seen_pubkey: Set<String> = Set()
-    let pool: RelayPool
-    let sub_id = UUID().description
+    let damus_state: DamusState
+    let base_subid = UUID().description
+    let profiles_subid = UUID().description
     let limit: UInt32 = 250
     
-    init(pool: RelayPool) {
-        self.pool = pool
+    init(damus_state: DamusState) {
+        self.damus_state = damus_state
     }
     
     func get_base_filter() -> NostrFilter {
-        var filter = NostrFilter.filter_text
+        var filter = NostrFilter.filter_kinds([1, 42])
         filter.limit = self.limit
         filter.until = Int64(Date.now.timeIntervalSince1970)
         return filter
@@ -31,12 +32,22 @@ class SearchHomeModel: ObservableObject {
     
     func subscribe() {
         loading = true
-        pool.subscribe(sub_id: sub_id, filters: [get_base_filter()], handler: handle_event)
+        damus_state.pool.subscribe(sub_id: base_subid, filters: [get_base_filter()], handler: handle_event)
     }
 
     func unsubscribe() {
         loading = false
-        pool.unsubscribe(sub_id: sub_id)
+        damus_state.pool.unsubscribe(sub_id: base_subid)
+    }
+    
+    func load_profiles(relay_id: String) {
+        var filter = NostrFilter.filter_profiles
+        let authors = find_profiles_to_fetch(profiles: damus_state.profiles, events: events)
+        filter.authors = authors
+        
+        if !authors.isEmpty {
+            damus_state.pool.subscribe(sub_id: profiles_subid, filters: [filter], handler: handle_event)
+        }
     }
     
     func handle_event(relay_id: String, conn_ev: NostrConnectionEvent) {
@@ -46,10 +57,10 @@ class SearchHomeModel: ObservableObject {
         case .nostr_event(let event):
             switch event {
             case .event(let sub_id, let ev):
-                guard sub_id == self.sub_id else {
+                guard sub_id == self.base_subid || sub_id == self.profiles_subid else {
                     return
                 }
-                if ev.kind == NostrKind.text.rawValue {
+                if ev.is_textlike && ev.should_show_event {
                     if seen_pubkey.contains(ev.pubkey) {
                         return
                     }
@@ -57,13 +68,50 @@ class SearchHomeModel: ObservableObject {
                     let _ = insert_uniq_sorted_event(events: &events, new_ev: ev) {
                         $0.created_at > $1.created_at
                     }
+                } else if ev.known_kind == .metadata {
+                    process_metadata_event(image_cache: damus_state.image_cache, profiles: damus_state.profiles, ev: ev)
                 }
             case .notice(let msg):
                 print("search home notice: \(msg)")
-            case .eose:
+            case .eose(let sub_id):
                 loading = false
+                
+                if sub_id == self.base_subid {
+                    load_profiles(relay_id: relay_id)
+                } else if sub_id == self.profiles_subid {
+                    damus_state.pool.unsubscribe(sub_id: self.profiles_subid)
+                }
+                
                 break
             }
         }
     }
+}
+
+func find_profiles_to_fetch_pk(profiles: Profiles, event_pubkeys: [String]) -> [String] {
+    var pubkeys = Set<String>()
+    
+    for pk in event_pubkeys {
+        if profiles.lookup(id: pk) != nil {
+            continue
+        }
+        
+        pubkeys.insert(pk)
+    }
+    
+    return Array(pubkeys)
+}
+    
+func find_profiles_to_fetch(profiles: Profiles, events: [NostrEvent]) -> [String] {
+    var pubkeys = Set<String>()
+    
+    for ev in events {
+        if profiles.lookup(id: ev.pubkey) != nil {
+            continue
+        }
+        
+        pubkeys.insert(ev.pubkey)
+    }
+    
+    return Array(pubkeys)
 }
