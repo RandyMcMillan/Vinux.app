d19f2d46f34472121207a0550db09c2b04143207
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-14 02:45:57 +0100

    DamusState.swift: sync with damus

--- App/Shared/Models/DamusState.swift
+++ App/Shared/Models/DamusState.swift
@@ -37,11 +37,11 @@ struct DamusState {
     let tips: TipCounter
     let profiles: Profiles
     let dms: DirectMessagesModel
-    
+
     var pubkey: String {
         return keypair.pubkey
     }
-    
+
     static var empty: DamusState {
         return DamusState.init(pool: RelayPool(), keypair: Keypair(pubkey: "", privkey: ""), likes: EventCounter(our_pubkey: ""), boosts: EventCounter(our_pubkey: ""), contacts: Contacts(), tips: TipCounter(our_pubkey: ""), profiles: Profiles(), dms: DirectMessagesModel())
     }
