cc55b6a02cfe2f8c6a52ed82cf8683c66525e4ab
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-15 20:51:54 +0100

    All iOS targets validate

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift at Aug 13 21:08:33 2022 +0100
+// AppInfo.swift on Aug 15 20:24:55 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "ead0d57"
+let gitCommitSHA: String = "fb78d30"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
