6aa24bf6f46efce6752336327cef42d64d8c1c39
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-15 19:21:29 +0100

    SearchHomeModel.swift: needs better Catalyst support

--- App/Shared/Models/SearchHomeModel.swift
+++ App/Shared/Models/SearchHomeModel.swift
@@ -69,7 +69,11 @@ class SearchHomeModel: ObservableObject {
                         $0.created_at > $1.created_at
                     }
                 } else if ev.known_kind == .metadata {
+                    #if !os(macOS)
                     process_metadata_event(image_cache: damus_state.image_cache, profiles: damus_state.profiles, ev: ev)
+                    #else
+                    process_metadata_event(profiles: damus_state.profiles, ev: ev)
+                    #endif
                 }
             case .notice(let msg):
                 print("search home notice: \(msg)")
