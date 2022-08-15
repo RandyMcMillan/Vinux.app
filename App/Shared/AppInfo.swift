ccde726d9f3a586d562bc4238756574c3b9f78ec
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-15 21:43:40 +0100

    Signing & Entitlements fix with commit notes
    Select use ios bundle id when publishing the Catalyst macOS bundle
    app.vinux.ios

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift on Aug 15 20:24:55 2022 +0100
+// AppInfo.swift on Aug 15 21:39:01 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "fb78d30"
+let gitCommitSHA: String = "f2680ce"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
