869e3bdea4dd7d98bfa9ac68da596c3ce655ae57
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-15 21:48:17 +0100

    macOS native validated on previous commit

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift on Aug 15 21:39:01 2022 +0100
+// AppInfo.swift on Aug 15 21:43:40 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "f2680ce"
+let gitCommitSHA: String = "ccde726"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
