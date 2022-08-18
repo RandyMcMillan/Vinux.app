45e1c61a189d0043614616225a2cfd0aa6e2a2b2
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-18 18:50:15 +0100

    App/Vinux.xcodeproj/project.pbxproj

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift ue Aug 16 08:09:10 2022 +0100
+// AppInfo.swift hu Aug 18 10:31:42 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "7d41469"
+let gitCommitSHA: String = "bdbbc35"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
