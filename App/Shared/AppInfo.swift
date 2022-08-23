572f562b48bed2557f4fe810e848d729bf9bd877
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-23 17:24:44 -0400

    App/Shared/Views/ConfigView.swift: Stack placement decision

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift ue Aug 23 14:50:33 2022 -0400
+// AppInfo.swift ue Aug 23 15:00:20 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "ac1ba9f"
+let gitCommitSHA: String = "7312722"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
