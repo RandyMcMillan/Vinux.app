8fe094a80a84c5db2fa28b99e6d4756a076b2123
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-24 01:08:40 -0400

    rolled back

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift ue Aug 23 20:15:14 2022 -0400
+// AppInfo.swift ri Aug 19 19:10:06 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "04ea1f5"
+let gitCommitSHA: String = "0ebc678"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
