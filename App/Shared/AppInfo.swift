04ea1f5acf4b2f3813d7cc5d075753bc3a45834a
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-23 20:15:14 -0400

    App/Shared/Views/ConfigView.swift: with Profile

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift ue Aug 23 15:00:20 2022 -0400
+// AppInfo.swift ue Aug 23 17:24:44 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "7312722"
+let gitCommitSHA: String = "572f562"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
