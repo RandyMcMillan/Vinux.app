119417ffe128e569434c01d84ec4a7a766b9cbd6
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-29 23:37:13 -0400

    checkpoint before reimplement sidenav

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift ue Aug 30 04:03:51 2022 +0100
+// AppInfo.swift on Aug 29 23:35:14 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "560fd95"
+let gitCommitSHA: String = "14f3761"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
