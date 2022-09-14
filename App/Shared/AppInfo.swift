9f0917caf06c182bee00478ccd7cbddac283c511
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-09-14 19:41:29 -0400

    SDWebImageMapKit/SDWebImage: remove unused package

--- App/Shared/AppInfo.swift
+++ App/Shared/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift on Aug 29 23:38:04 2022 -0400
+// AppInfo.swift hu Sep 15 00:30:50 2022 +0100
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "119417f"
+let gitCommitSHA: String = "e37eac0"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
